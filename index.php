3a57eacef44c0e58b7e6168f4024c8ffcd4dc4a4
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-09 18:18:22 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -5,7 +5,22 @@
 </head>
 <body>
 <form>
-    hi
+    <div style="width:500px; height:400px; float:center; border:1px; background-color:green">
+        <p>
+            <strong>아이디</strong>
+            <input type="text" name="id" value="아이디 입력">
+        </p>
+        
+        <p>
+            <strong>비밀번호</strong>
+            <input type="password" name="password" value="비밀번호 입력">
+        </p>
+        
+        <p>
+            <strong>이름</strong>
+            <input type="text" name="name" value="이름 입력">
+        </p>
+    </div>
 </form>
 </body>
 </html>
