8b1070585d7a8e92139b9ec801b3cf72671bcc3f
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-09 17:44:29 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,118 +1,135 @@
-<?php
-session_start();
-require_once "./iccc/config.php";
-//Check Mobile
-$mAgent = array("iPhone","iPod","Android","Blackberry","Opera Mini", "Windows ce", "Nokia", "sony" );
-$isMobile = false;
-for($i=0; $i<sizeof($mAgent); $i++){
-	if(stripos( $_SERVER['HTTP_USER_AGENT'], $mAgent[$i] )){
-		$isMobile = true;
-		break;
-	}
-}
-	
-if ($isMobile) {
-	$title = $site_name_abbr;
-}
-else {
-	$title = $site_name_full;
-}
-
-if (!isset($_GET['page'])) {
-	$content_url = $base_folder."/"."about.php";
-}
-else {
-	if ($_GET['page'] == "bbslist" ) $content_url = "./bbs/".$_GET['page'].".php";
-	else $content_url = $base_folder."/".$_GET['page'].".php";
-	$active_str = "class='active'";
-	switch($_GET['page']) {
-		case 'about': $about_class_str = $active_str; break;
-		case 'certificate': $certificate_class_str = $active_str; break;
-		case 'education': $education_class_str = $active_str; break;
-		case 'partner': $partner_class_str = $active_str; break;
-		case 'contact': $contact_class_str = $active_str; break;
-		case 'bbslist': $bbslist_class_str = $active_str; break;
-		case 'login': $login_class_str = $active_str; break;
-	}
-}
-?>
 <!doctype html>
 <html>
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
-    <!-- Latest compiled and minified CSS -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-
-    <title><?=$site_name_abbr?></title>
+<meta charset="utf-8">
+<title>이력서</title>
 </head>
 
 <body>
-    <div class="container-fluid ">
-    
-    <h2 style="text-align:center;"><?=$title?></h2>
-    
-    <nav class="navbar navbar-default">
-    <div class="container-fluid">
-    <div class="navbar-header">
-    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-    <span class="icon-bar"></span>
-    <span class="icon-bar"></span>
-    <span class="icon-bar"></span>
-    </button>
-    <a href="./index.php" target="_self"><img src='<?=$base_folder?>/image/iccc_logo.png' style='width:50px; vertical-align:bottom; float:left; padding-top:10px;'></a>
-    <!--<a class="navbar-brand" href="./index.php"><?=$licence_name_abbr?></a> -->
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-    
-    <ul class="nav navbar-nav">
-    <li <?=$about_class_str?>><a href="index.php?page=about">About <?=$licence_name_abbr?></a></li>
-    <li <?=$certificate_class_str?>><a href="index.php?page=certificate">CERTIFICATE</a></li>
-    <li <?=$education_class_str?>><a href="index.php?page=education">EDUCATION</a></li>
-    <li <?=$partner_class_str?>><a href="index.php?page=partner">PARTNER</a></li>
-    <li <?=$contact_class_str?>><a href="index.php?page=contact">CONTACT</a></li>
-    <li <?=$bbslist_class_str?>><a href="index.php?page=bbslist">BOARD</a></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-<?php
-	if ($_SESSION['ss_login_status'] == '') {
-?>
-    <li><a href="index.php?page=login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-<?php
-	}
-	else if ($_SESSION['ss_login_status'] == 'logged_in') {
-?>
-    <li><a href="index.php?page=login&act=logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
-<?php
-	}
-?>
-    </ul>
-    </div>
-    </div>
-    </nav>
-
-    <div >
-<?php
-	require_once $content_url ;
-?>
-    </div>
-<?php
-	require_once $base_folder ."/" ."footer.php" ;
-?>
-    </div>
+<h1 style="text-align: center"><strong>이  력  </strong>서<br>
+  <br>
+</h1>
+<p><strong>1.인적사항</strong></p>
+<table width="100%" height="180" border="1" style="text-align: center">
+  <tbody>
+    <tr>
+      <td width="120" rowspan="5"><img src="쥬노.jpg" width="105" height="141" alt=""/></td>
+      <td width="53" rowspan="3" bgcolor="#BBBBBB">성 명</td>
+      <td width="56" height="33" bgcolor="#BBBBBB">한 글</td>
+      <td width="134">이희수</td>
+      <td width="103" bgcolor="#BBBBBB">주민등록번호</td>
+      <td width="127">970211-1</td>
+    </tr>
+    <tr>
+      <td width="56" height="34" bgcolor="#BBBBBB">한 문</td>
+      <td width="134">李曦洙</td>
+      <td width="103" bgcolor="#BBBBBB">생년월일</td>
+      <td width="127">970211</td>
+    </tr>
+    <tr>
+      <td width="56" height="36" bgcolor="#BBBBBB">영 문</td>
+      <td width="134">Lee Heesu</td>
+      <td width="103" bgcolor="#BBBBBB">성 별</td>
+      <td width="127">남</td>
+    </tr>
+    <tr>
+      <td width="53" rowspan="2" bgcolor="#BBBBBB">연락처</td>
+      <td width="56" height="30" bgcolor="#BBBBBB">자 택</td>
+      <td width="134">x</td>
+      <td width="103" bgcolor="#BBBBBB">e-mail</td>
+      <td width="127">gmltn6419@naver.com</td>
+    </tr>
+    <tr>
+      <td width="56" height="35" bgcolor="#BBBBBB">핸드폰</td>
+      <td width="134">010-3949-0818</td>
+      <td width="103" bgcolor="#BBBBBB">보훈여부</td>
+      <td width="127">□ Y   □ N</td>
+    </tr>
+  </tbody>
+</table>
+<p><strong>2.학력사항</strong></p>
+<table width="100%" height="228" border="1" style="text-align: center">
+  <tbody>
+    <tr>
+      <td width="112" height="20" bgcolor="#BBBBBB">학교명</td>
+      <td width="84" height="20" bgcolor="#BBBBBB">전   공  </td>
+      <td width="95" height="20" bgcolor="#BBBBBB">입학일자</td>
+      <td width="96" height="20" bgcolor="#BBBBBB">졸업일자</td>
+      <td width="145" height="20" bgcolor="#BBBBBB">졸업구분</td>
+      <td width="60" height="20" bgcolor="#BBBBBB">소 재 지</td>
+    </tr>
+    <tr>
+      <td width="112" height="20">진해고등학교</td>
+      <td width="84" height="20">인문계</td>
+      <td width="95" height="20">&nbsp;</td>
+      <td width="96" height="20">&nbsp;</td>
+      <td width="145" height="20" align="left"><p>□ 졸업 □ 예정</p></td>
+      <td width="60" height="20">&nbsp;</td>
+    </tr>
+    <tr>
+      <td width="112" height="20">신라대학교</td>
+      <td width="84" height="20">컴퓨터소프트웨어 공학부</td>
+      <td width="95" height="20">2015</td>
+      <td width="96" height="20">&nbsp;</td>
+      <td width="145" height="20" align="left"><p>□ 졸업 □ 예정 □ 수료</p></td>
+      <td width="60" height="20">&nbsp;</td>
+    </tr>
+    <tr>
+      <td width="112" height="20">&nbsp;</td>
+      <td width="84" height="20">&nbsp;</td>
+      <td width="95" height="20">&nbsp;</td>
+      <td width="96" height="20">&nbsp;</td>
+      <td width="145" height="20" align="left"><p>□ 졸업 □ 예정 □ 수료</p></td>
+      <td width="60" height="20">&nbsp;</td>
+    </tr>
+    <tr>
+      <td width="112" height="20">&nbsp;</td>
+      <td width="84" height="20">&nbsp;</td>
+      <td width="95" height="20">&nbsp;</td>
+      <td width="96" height="20">&nbsp;</td>
+      <td width="145" height="20" align="left"><p>□ 졸업 □ 예정 □ 수료</p></td>
+      <td width="60" height="20">&nbsp;</td>
+    </tr>
+    <tr>
+      <td width="112" height="20">&nbsp;</td>
+      <td width="84" height="20">&nbsp;</td>
+      <td width="95" height="20">&nbsp;</td>
+      <td width="96" height="20">&nbsp;</td>
+      <td width="145" height="20" align="left"><p>□ 졸업 □ 예정 □ 수료</p></td>
+      <td width="60" height="20">&nbsp;</td>
+    </tr>
+  </tbody>
+</table>
+<p><strong>3.자격사항</strong></p>
+<table width="100%" height="174" border="1">
+  <tbody>
+    <tr>
+      <td width="256" align="center" valign="middle" bgcolor="#BBBBBB">자격증명</td>
+      <td width="83" align="center" valign="middle" bgcolor="#BBBBBB">등 급</td>
+      <td width="132" align="center" valign="middle" bgcolor="#BBBBBB">취 득 일</td>
+      <td width="133" align="center" valign="middle" bgcolor="#BBBBBB">발급기관</td>
+    </tr>
+    <tr>
+      <td align="center" valign="middle">&nbsp;</td>
+      <td align="center" valign="middle">&nbsp;</td>
+      <td align="center" valign="middle">&nbsp;</td>
+      <td align="center" valign="middle">&nbsp;</td>
+    </tr>
+    <tr>
+      <td align="center" valign="middle">&nbsp;</td>
+      <td align="center" valign="middle">&nbsp;</td>
+      <td align="center" valign="middle">&nbsp;</td>
+      <td align="center" valign="middle">&nbsp;</td>
+    </tr>
+    <tr>
+      <td align="center" valign="middle">&nbsp;</td>
+      <td align="center" valign="middle">&nbsp;</td>
+      <td align="center" valign="middle">&nbsp;</td>
+      <td align="center" valign="middle">&nbsp;</td>
+    </tr>
+  </tbody>
+</table>
+<p style="text-align: center"><strong>작성일 : 2019년  3월  10일</strong></p>
+<p style="text-align: center"><strong>작성자 : 이희수 (인)</strong></p>
 </body>
 </html>
-<script>
-	window .setTimeout (function () {
-		$(".alert").fadeTo(500, 0).slideUp(500, function(){
-		$(this).remove();});
-		}, 4000);
-	
-	function confirmAction() {
-		if (confirm("Are you sure to continue?")) return true;
-		else return false;
-	
-	}
-</script>
