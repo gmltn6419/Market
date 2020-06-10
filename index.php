9a4bbda339d8b462396a3da44e8066cce1d83325
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-10 15:44:07 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ catch (PDOException $e) {
 $connectionInfo = array("UID" => "heesu", "pwd" => "dlvlwk12@", "Database" => "Market", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:market01.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-
+/*
 $serverName = "tcp:market01.database.windows.net,1433"; // update me
 $connectionOptions = array(
     "Database" => "Market", // update me
@@ -23,6 +23,7 @@ $connectionOptions = array(
 );
 //Establishes the connection
 $conn = sqlsrv_connect($serverName, $connectionOptions);
+*/
 $tsql= "SELECT id, pw
         FROM member
         WHERE id = 'gmltn6419'";
