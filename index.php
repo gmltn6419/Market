dcd95e4b96117f0f13e5aa6bb0ba78cc1e6d085b
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-10 20:12:24 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,35 +1,69 @@
 <?php
-# Setup a specific instance of an Azure::Storage::Client
-    $connectionString = "DefaultEndpointsProtocol=https;AccountName=".getenv('heesu').";AccountKey=".getenv('kvTvbmEjep6bcqnWX7Ggq1QimeeoHtL75Ikq1PBdLPZW6EznTe/LF55kl2PTgf2t0nobqrlsn80OFu56bKvXSQ==');
-    
-    // Create blob client.
-    $blobClient = BlobRestProxy::createBlobService($connectionString);
-    
-    # Create the BlobService that represents the Blob service for the storage account
-    $createContainerOptions = new CreateContainerOptions();
-    
-    $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
-    
-    // Set container metadata.
-    $createContainerOptions->addMetaData("key1", "value1");
-    $createContainerOptions->addMetaData("key2", "value2");
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:market01.database.windows.net,1433; Database = Market", "heesu", "dlvlwk12@");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
-    $containerName = "blockblobs".generateRandomString();
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "heesu", "pwd" => "dlvlwk12@", "Database" => "Market", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:market01.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+/*
+$serverName = "tcp:market01.database.windows.net,1433"; // update me
+$connectionOptions = array(
+    "Database" => "Market", // update me
+    "Uid" => "heesu", // update me
+    "PWD" => "dlvlwk12@" // update me
+);
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+*/
+$tsql= "SELECT id, pw
+        FROM member
+        WHERE id = 'gmltn6419'";
 
-    try    {
-        // Create container.
-        $blobClient->createContainer($containerName, $createContainerOptions);
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['id'] . " " . $row['pw'] . PHP_EOL);
+     $id = $row['id'];
+     $pw = $row['pw'];
+    }
+    sqlsrv_free_stmt($getResults);
+?>
 
-    $myfile = fopen("HelloWorld.txt", "w") or die("Unable to open file!");
-    fclose($myfile);
-
-    # Upload file as a block blob
-    echo "Uploading BlockBlob: ".PHP_EOL;
-    echo $fileToUpload;
-    echo "<br />";
-    
-    $content = fopen($fileToUpload, "r");
-
-    //Upload blob
-    $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
-    ?>
+<!doctype html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
+</head>
+<body>
+<form>
+    <div align="center" style="width:500px; height:400px; float:center; border:1px; background-color:green">
+        <p align="center">
+            <strong>아이디</strong>
+            <input type="text" name="id" value="<?=$id?>">
+        </p>
+        
+        <p align="center">
+            <strong>비밀번호</strong>
+            <input type="password" name="pw" value="<?=$pw?>">
+        </p>
+        
+        <p align="center">
+            <strong>이름</strong>
+            <input type="text" name="name" value="이름 입력">
+        </p>
+        <p align="center">
+        <button type="button" onClick="location.href='test.php'">test</button>
+    </div>
+</form>
+</body>
+</html>
