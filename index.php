eb294961cd56a91b1f6a3347dfe3b86c1ffe6d6f
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-10 15:41:10 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,45 +1,5 @@
-<?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:market01.database.windows.net,1433; Database = Market", "heesu", "dlvlwk12@");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "heesu", "pwd" => "dlvlwk12@", "Database" => "Market", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:market01.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-
-$serverName = "tcp:market01.database.windows.net,1433"; // update me
-$connectionOptions = array(
-    "Database" => "Market", // update me
-    "Uid" => "heesu", // update me
-    "PWD" => "dlvlwk12@" // update me
-);
-//Establishes the connection
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-$tsql= "SELECT id, pw
-        FROM member
-        WHERE id = 'gmltn6419'";
-
-    $getResults= sqlsrv_query($conn, $tsql);
-    //echo ("Reading data from table" . PHP_EOL);
-    if ($getResults == FALSE)
-        echo (sqlsrv_errors());
-    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-     //echo ($row['id'] . " " . $row['pw'] . PHP_EOL);
-     $id = $row['id'];
-     $pw = $row['pw'];
-    }
-    sqlsrv_free_stmt($getResults);
-    sqlscv_close( $conn);
-?>
-
-//<!doctype html>
+<!doctype html>
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
