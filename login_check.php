3a5447a16a66fc6de42e6d08a46e21366ba68d85
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-11 17:08:43 +0900

    Add files via upload

--- login_check.php
+++ login_check.php
@@ -31,23 +31,17 @@ if ($getResults == FALSE)
     echo (sqlsrv_errors());
 
 while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-
-    if($getResults->num_rows==1){
-        if($row['pw']==$pw){
-            $_SESSION['userid']=$id;
-            if(isset($_SESSION['userid'])){
-                header('Location :./index.php'); // 로그인 성공 시 페이지 이동
-            }
-            else{
-                echo "세션 저장 실패";
-            }
+    if($row['pw']==$pw){
+        $_SESSION['userid']=$id;
+        if(isset($_SESSION['userid'])){
+            header('Location: ./index.php'); // 로그인 성공 시 페이지 이동
         }
         else{
-            echo "wrong id or pw";
+            echo "세션 저장 실패";
         }
     }
     else{
-        echo "wrong id or pw";
+        echo "wrong id or pw1";
     }
 }
 
