c7b268d0a743d1d582f0a76715a4cda7fabab866
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-11 19:31:05 +0900

    Add files via upload

--- write.php
+++ write.php
@@ -50,7 +50,7 @@
         var dbLat = latlng.getLat();
         var dbLng = latlng.getLng();
 
-        document.getElementById("dbLat").value = 123.34567;
+        document.getElementById("dbLat").value = "123.34567";
 
         var message = '위도 : ' + dbLat + ' ';
         message += '경도 : ' + dbLng;
