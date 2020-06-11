595181a48cc44ec89d354724a41b2e3bebd8e046
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-11 19:41:09 +0900

    Add files via upload

--- write2.php
+++ write2.php
@@ -1,16 +1,16 @@
 <?php
 session_start();
-/*
+
 $filename =  date("YmdHis").".jpg";
 move_uploaded_file($_FILES['imageform']['tmp_name'], $filename);
-*/
 
-//$title=$_POST['title'];
-//$price=$_POST['price'];
+
+$title=$_POST['title'];
+$price=$_POST['price'];
 $dbLat = $_POST["dbLat"];
 
-//echo $title "<br>";
-//echo $price "<br>";
+echo $title "<br>";
+echo $price "<br>";
 echo $dbLat "<br>";
-//echo $Lng "<br>";
+echo $Lng "<br>";
 ?>
