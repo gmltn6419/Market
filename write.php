502760fd79406721db5ff1e580ee2e734da71626
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-11 19:27:12 +0900

    Add files via upload

--- write.php
+++ write.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 <form enctype="multipart/form-data" name="form" method="post" action="write2.php">
-    <!-- <input type="hidden" name="dbLat" id="dbLat" /> -->
+    <input type="hidden" name="dbLat" id="dbLat" />
     사진 : <input type="file" name="image" />
     <br>
     제목 : <input type="text" name="title" placeholder="제목을 입력하세요!" style="width:300px">
@@ -18,6 +18,7 @@
     <p><em>지도를 클릭해주세요!</em></p> 
     <div id="map" style="width:50%;height:350px;"></div>
     <div id="clickLatlng"></div>
+ 
     <script>
     var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
         mapOption = { 
@@ -39,23 +40,23 @@
     // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
     kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
     
-    // 클릭한 위도, 경도 정보를 가져옵니다 
-    var latlng = mouseEvent.latLng; 
-    
-    // 마커 위치를 클릭한 위치로 옮깁니다
-    marker.setPosition(latlng);
-    
-    //db저장
-    var dbLat = latlng.getLat();
-    var dbLng = latlng.getLng();
+        // 클릭한 위도, 경도 정보를 가져옵니다 
+        var latlng = mouseEvent.latLng; 
+        
+        // 마커 위치를 클릭한 위치로 옮깁니다
+        marker.setPosition(latlng);
+        
+        //db저장
+        var dbLat = latlng.getLat();
+        var dbLng = latlng.getLng();
 
-    //document.getElementById("dbLat").value = dbLat;
+        document.getElementById("dbLat").value = 123.34567;
 
-    var message = '위도 : ' + dbLat + ' ';
-    message += '경도 : ' + dbLng;
-    
-    var resultDiv = document.getElementById('clickLatlng'); 
-    resultDiv.innerHTML = message;  
+        var message = '위도 : ' + dbLat + ' ';
+        message += '경도 : ' + dbLng;
+        
+        var resultDiv = document.getElementById('clickLatlng'); 
+        resultDiv.innerHTML = message;  
     });
     </script>
     <br>
