10046cc008d3f56c076dcc44ad13cc590e050c2b
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-11 18:13:32 +0900

    Add files via upload

--- write.php
+++ write.php
@@ -15,52 +15,18 @@ session_start();
     가격 : <input type="text" name="price" placeholder="가격을 입력하세요!" style="width:300px">
     <br>
     <div id="map" style="width:100%;height:350px;"></div>
-    <p><em>지도를 클릭해주세요!</em></p> 
-    <div id="clickLatlng"></div>
     <script>
     var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-    mapOption = { 
-        center: new kakao.maps.LatLng(35.16825697799745, 128.99625354800833), // 지도의 중심좌표(신라대)
-        level: 3 // 지도의 확대 레벨
-    };
+        mapOption = { 
+            center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+            level: 3 // 지도의 확대 레벨
+        };
 
-    var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
-
-    // 지도를 클릭한 위치에 표출할 마커입니다
-    var marker = new kakao.maps.Marker({ 
-    // 지도 중심좌표에 마커를 생성합니다 
-    position: map.getCenter() 
-    }); 
-    // 지도에 마커를 표시합니다
-    marker.setMap(map);
-
-    // 지도에 클릭 이벤트를 등록합니다
-    // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
-    kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
-
-    // 클릭한 위도, 경도 정보를 가져옵니다 
-    var latlng = mouseEvent.latLng; 
-
-    // 마커 위치를 클릭한 위치로 옮깁니다
-    marker.setPosition(latlng);
-
-    //db저장
-    var dbLat = latlng.getLat(); //위도
-    var dbLng = latlng.getLng(); //경도
-    
-    dbLat = <?=$_SESSION['dbLat']?>;
-    dbLng = <?=$_SESSION['dbLng']?>;
-
-    var message = '위도 : ' + dbLat + ' ';
-    message += '경도 : ' + dbLng;
-
-    var resultDiv = document.getElementById('clickLatlng'); 
-    resultDiv.innerHTML = message;
-
-    });
+    // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+    var map = new kakao.maps.Map(mapContainer, mapOption);
     </script>
-<br>
-<input type="submit" value="전송" />
+    <br>
+    <input type="submit" value="전송" />
 </form>
 </body>
 </html>
