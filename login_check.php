04486f9648dfb46a571dfd8f034866b28aa27504
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-11 17:21:16 +0900

    Add files via upload

--- login_check.php
+++ login_check.php
@@ -20,30 +20,31 @@ $connectionInfo = array("UID" => "heesu", "pwd" => "dlvlwk12@", "Database" => "M
 $serverName = "tcp:market01.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-$tsql= "SELECT *
-        FROM member
-        WHERE id = '$id'";
+$tsql= "SELECT id
+        FROM member;"
 
 $getResults= sqlsrv_query($conn, $tsql);
 
 echo ("Reading data from table" . PHP_EOL);
 
-if ($getResults == FALSE){
-    echo "세션 저장 실패";
-    exit();
-}
-
 while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-    if($row['pw']==$pw){
-        $_SESSION['userid']=$id;
-        if(isset($_SESSION['userid'])){
-            header('Location: ./index.php'); // 로그인 성공 시 페이지 이동
-        }
-        else{
-            echo "세션 저장 실패";
-            exit();
+    if($row['id']!=$id){
+        echo "세션 저장 실패";
+        exit();
+    }
+    else{
+        if($row['pw']==$pw){
+            $_SESSION['userid']=$id;
+            if(isset($_SESSION['userid'])){
+                header('Location: ./index.php'); // 로그인 성공 시 페이지 이동
+            }
+            else{
+                echo "세션 저장 실패";
+                exit();
+            }
         }
     }
+    
 }
 
 sqlsrv_free_stmt($getResults);
