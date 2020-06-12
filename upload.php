b2065bf5c282837dbf40ab9b271024c3aff1ee50
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-12 22:08:51 +0900

    Add files via upload

--- upload.php
+++ upload.php
@@ -8,6 +8,7 @@ $filename =  date("YmdHis").".jpg";
 move_uploaded_file($_FILES['imageform']['tmp_name'], $filename);
 
 $title=$_POST['title'];
+echo $_POST['title'];
 $price=$_POST['price'];
 $dbLat = $_POST['dbLat'];
 $dbLng = $_POST['dbLng'];
