b396fb3586c4c944b1061acad8a2aef5b88c194e
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-12 18:16:24 +0900

    Add files via upload

--- direct.php
+++ direct.php
@@ -8,6 +8,8 @@ catch (PDOException $e) {
     die(print_r($e));
 }
 
+$theVariable = array();
+
 $connectionInfo = array("UID" => "heesu", "pwd" => "dlvlwk12@", "Database" => "Market", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:market01.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
@@ -19,7 +21,9 @@ echo ("Reading data from table" . PHP_EOL);
 if ($getResults == FALSE)
     echo (sqlsrv_errors());
 while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-    $filename = $row['filename'];
+    for(int i=0; i<=$row.length(); i++){
+        $theVariable = $row['filename'];
+    }
 }
 sqlsrv_free_stmt($getResults);
 
@@ -27,17 +31,21 @@ $connectionInfo = array("UID" => "heesu", "pwd" => "dlvlwk12@", "Database" => "M
 $serverName = "tcp:market01.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
+$theVariable2 = array();
+$theVariable3 = array();
+
 $tsql2= "SELECT Lat, Lng FROM product";
 $getResults2= sqlsrv_query($conn, $tsql2);
 
 echo ("Reading data from table" . PHP_EOL);
 if ($getResults2 == FALSE)
     echo (sqlsrv_errors());
+
 while ($row2 = sqlsrv_fetch_array($getResults2, SQLSRV_FETCH_ASSOC)) {
-    echo ($row2['filename'] . PHP_EOL);
-    //$path = $row2['path'];
-    $Lat = $row2['Lat'];
-    $Lng = $row2['Lng'];
+    for(int i=0; i<=$row2.length(); i++){
+        $theVariable2 = $row['Lat'];
+        $theVariable3 = $row['Lng'];
+    }
 }
 sqlsrv_free_stmt($getResults2);
 ?>
@@ -59,7 +67,35 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-var imageSrc = '<?=$filename;?>', // 마커이미지의 주소입니다    
+
+for (var i = 0; i < <?=$theVariable2?>.length; i ++) {
+    var positions[i] = latlng: new kakao.maps.LatLng(<?=$theVariable2[i]?>,<?=$theVariable3[i]?>);
+}
+
+for (var i = 0; i < <?=$theVariable?>.length; i ++) {
+    var imageSrc[i] = <?=$theVariable2[i]?>;
+}
+
+
+for (var i = 0; i < positions.length; i ++) {
+    
+    // 마커 이미지의 이미지 크기 입니다
+    var imageSize = new kakao.maps.Size(24, 35); 
+    
+    // 마커 이미지를 생성합니다    
+    var markerImage = new kakao.maps.MarkerImage(imageSrc[i], imageSize); 
+    
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        map: map, // 마커를 표시할 지도
+        position: positions[i].latlng, // 마커를 표시할 위치
+        image : markerImage // 마커 이미지 
+    });
+}
+
+
+/*
+var imageSrc = '<?=$filename[];?>', // 마커이미지의 주소입니다    
     imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
     imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
       
@@ -75,6 +111,7 @@ var marker = new kakao.maps.Marker({
 
 // 마커가 지도 위에 표시되도록 설정합니다
 marker.setMap(map);  
+*/
 </script>
 </form>
 <button style="height:100px; width:100px; font-size: 25px;" onclick="location.href='./write.php'"> 글 작성 </button>
