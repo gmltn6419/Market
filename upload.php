2a55c13c641cfbd05fa283ccce6c4950848ef9dd
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-12 17:07:38 +0900

    Add files via upload

--- upload.php
+++ upload.php
@@ -29,7 +29,7 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
 //$path = $_SERVER['DOCUMENT_ROOT'].'/testBBS/';
 //$path = "./";
 
-$tsql= "INSERT INTO image (filename) VALUES ('$filename')";
+$tsql= "INSERT INTO product VALUES ('$title','$price','$dbLat','$dbLng','$id')";
 
 $getResults= sqlsrv_query($conn, $tsql);
 echo ("Reading data from table" . PHP_EOL);
@@ -40,6 +40,7 @@ while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
     //$id = $row['id'];
     //$pw = $row['pw'];
 }
+sqlsrv_free_stmt($getResults);
 
 $connectionInfo = array("UID" => "heesu", "pwd" => "dlvlwk12@", "Database" => "Market", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:market01.database.windows.net,1433";
@@ -48,16 +49,16 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
 //$path = $_SERVER['DOCUMENT_ROOT'].'/testBBS/';
 //$path = "./";
 
-$tsql2= "INSERT INTO product VALUES ('$title','$price','$dbLat','$dbLng','$id')";
+$tsql2= "INSERT INTO image (filename) VALUES ('$filename')";
 
 $getResults2= sqlsrv_query($conn, $tsql2);
 echo ("Reading data from table" . PHP_EOL);
 if ($getResults2 == FALSE)
     echo (sqlsrv_errors());
 while ($row = sqlsrv_fetch_array($getResults2, SQLSRV_FETCH_ASSOC)) {
-    alert("글 작성 완료!");
+    echo "글 작성 완료!"
     echo "<a href=./index.php>back page</a>";
 }
-sqlsrv_free_stmt($getResults);
+
 sqlsrv_free_stmt($getResults2);
 ?>
