afb3c35b635b09c6abe1ff6fba260bbbe5d9352e
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-12 15:40:48 +0900

    Add files via upload

--- write.php
+++ write.php
@@ -8,8 +8,8 @@
 </head>
 <body>
 <form enctype="multipart/form-data" name="form" method="post" action="./write2.php">
-    <!-- <input type="hidden" name="dbLat" id="dbLat" /> -->
-    사진 : <input type="file" name="image" />
+    <!-- <input type="hidden" name="dbLat" id="dbLat" /> 
+    사진 : <input type="file" name="image" /> -->
     <br>
     제목 : <input type="text" name="title" placeholder="제목을 입력하세요!" style="width:300px">
     <br>
