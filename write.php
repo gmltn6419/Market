3cf9f77c3dea54bb07f3c30e96a9882c47315f31
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-12 22:19:56 +0900

    Add files via upload

--- write.php
+++ write.php
@@ -1,16 +1,6 @@
 <?php
     header("Content-Type: text/html; charset=UTF-8");
     session_start();
-
-    $str = "테스트";
-
-    $encode = array('ASCII','UTF-8','EUC-KR');
-
-    $str_encode = mb_detect_encoding($str, $encode);
-
-    if(strtoupper($str_encode) == 'UTF-8') {
-        echo 'UTF-8 입니다';
-    }
 ?>
 <!DOCTYPE html>
 <html>
