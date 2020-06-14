42326fb7a4692604537dac3cdd2d4043c14a79a5
gmltn6419 <56532490+gmltn6419@users.noreply.github.com>
2020-06-14 21:06:00 +0900

    Add files via upload

--- coupon.php
+++ coupon.php
@@ -4,7 +4,9 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 </head>
 <body>
-<button style="height:100px; width:100px; font-size: 25px;" onclick="location.href='./write2.php'"> 쿠폰등록 </button>
+<div align="center">
+<button style="height:100px; width:100px; font-size: 20px;" onclick="location.href='./write2.php'"> 쿠폰등록 </button>
 <button style="height:100px; width:100px; font-size: 20px;" onclick="location.href='./index.php'"> 메인화면 </button>
+</div>
 </body>
 </html>
--- write2.php
+++ write2.php
@@ -9,6 +9,7 @@
 </head>
 <body>
 <form enctype="multipart/form-data" name="form" method="post" action="upload2.php">
+<div align="center">
     쿠폰번호 : <input type="text" name="cno" placeholder="사용처를 입력하세요!" style="width:300px">
     <br>
     사용처 : <input type="text" name="cname" placeholder="사용처를 입력하세요!" style="width:300px">
@@ -16,6 +17,7 @@
     위치 : <input type="text" name="location" placeholder="위치를 입력하세요!" style="width:300px">
     <br>
     <input type="submit" value="등록" />
+</div>
 </form>
 </body>
 </html>
